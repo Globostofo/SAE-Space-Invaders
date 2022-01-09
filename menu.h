4c63d46b5a5c2cf08451c3a8a46df69e4593015a
lucaceccarelli <91269970+lucaceccarelli@users.noreply.github.com>
2022-01-09 20:52:13 +0100

    Update menu.h

--- menu.h
+++ menu.h
@@ -3,14 +3,16 @@
 
 /*!
   * @author Ceccarelli Luca - Clement Romain - Saadi Nils - Valls Marion
-  * @date 13 dec 2021
+  * @date 9 jan 2021
   * @brief fichier permettant de créer des menus pour commencer le jeu
 **/
 
 #define FPS_LIMIT 60
 
 #include <iostream>
-#include <thread>
+#include <thread>   //gerer les frames et le temps
+#include <string>   //convert char type to string
+#include <map>      //store game themes
 
 #include "mingl/shape/rectangle.h"
 #include "mingl/mingl.h"
@@ -21,14 +23,15 @@
 
 using namespace std;
 
-
-//nsGraphics::Vec2D rectPos;
-//nsGraphics::RGBAcolor rectColor = nsGraphics::KCyan;
-
+/*!
+  * @brief structure contenant l'element button
+  * @fn button{string content, int buttonId,  nsGraphics::RGBAcolor bgColor,  nsGraphics::RGBAcolor borderColor,  nsGraphics::RGBAcolor textColor }
+**/
 struct button{
     //texte a afficher et couleur dans laquelle l'afficher
 
     string content;
+    int buttonId;
     nsGraphics::RGBAcolor bgColor;
     nsGraphics::RGBAcolor borderColor;
     nsGraphics::RGBAcolor textColor;
@@ -43,12 +46,73 @@ struct button{
                             nsGraphics::Vec2D{0,0},
                             bgColor,
                             borderColor};
-};
+};//button
 
-void events(MinGL &window,vector<button> &b);
+/*!
+  * @brief fonction pour verifier si on appuye sur la fenetre de jeu et si un bouton est concerné
+  * @param[in/out] EventManager
+  * @param[in] btns : vecteur contenant les boutons
+  * @fn int events(nsEvent::EventManager &eventM, const vector<button> &btns)
+**/
+int events(nsEvent::EventManager &eventM, const vector<button> &btns);
 
-void placerBoutons(MinGL & window, vector<button> & b);
+/*!
+  * @brief fonction pour recuperer la derniere touche qui as été appuyé sur le clavier
+  * @param[in/out] window
+  * @fn char getLastPressedChar(MinGL &window)
+**/
+char getLastPressedChar(MinGL &window);
 
+/*!
+  * @brief fonction pour placer des boutons centrés horizontalement et verticalement
+  * @param[in/out] window : fenetre
+  * @param[in] b : vecteur de struct contenant le texte à afficher et la couleur dans laquelle l'afficher
+  * @param[in] width : largeur de la fenetre
+  * @param[in] height : hauteur de la fenetre
+**/
+void placeBtns(const MinGL &window, vector<button> &btns);
+
+/*!
+  * @brief fonction utilisé pour afficher les boutons dans la fenetre : window
+  * @param[in/out] window
+  * @param[in] btns : vecteur contenant la liste des boutons à afficher
+  * @fn void drawBtns(MinGL &window, const vector<button> &btns)
+**/
+void drawBtns(MinGL &window, const vector<button> &btns);
+
+/*!
+  * @brief fonction utilisé pour commencer le jeu
+  * @param[in/out] window
+  * @fn void startButton(MinGL &window
+**/
+void startButton(MinGL &window);
+
+/*!
+  * @brief fonction acceder au menu qui affiche la liste des meilleurs scores
+  * @param[in/out] window
+  * @fn void scoreButton(MinGL &window)
+**/
+void scoreButton(MinGL &window);
+
+/*!
+  * @brief fonction utilisé pour acceder au menu des reglages
+  * @param[in/out] window
+  * @fn void settingsButton(MinGL &window)
+**/
+void settingsButton(MinGL &window);
+
+/*!
+  * @brief fonction qui sert à verifier quel bouton as été appuyé sur le menu principale, pour changer de fenetre
+  * @param[in/out] window : fenetre
+  * @param[in/out] content : contenu du bouton appuyé
+  * @fn void checkContent(MinGL &window, int &content)
+**/
+void checkContent(MinGL &window, int &content);
+
+/*!
+  * @brief fonction pour lancer le menu principale
+  * @fn void menu()
+**/
 void menu();
 
 #endif // MENU_H
