6cc14674e4bcf5fda4efcc43396e9b5712aa894a
GLOBOSTOFO <71722881+GLOBOSTOFO@users.noreply.github.com>
2022-01-10 16:53:47 +0100

    Minor corrections

--- menu.cpp
+++ menu.cpp
@@ -1,21 +1,8 @@
-/*!
-  * @author Ceccarelli Luca - Clement Romain - Saadi Nils - Valls Marion
-  * @date 9 jan 2021
-  * @brief fichier permettant de créer des menus pour commencer le jeu
-**/
-
-#include "menu.h"   //all the functions and the button struct
+#include "menu.h"
 
 using namespace std;
 
-/*!
-  * @brief fonction pour verifier si on appuye sur la fenetre de jeu et si un bouton est concerné
-  * @param[in/out] EventManager
-  * @param[in] btns : vecteur contenant les boutons
-  * @fn int events(nsEvent::EventManager &eventM, const vector<button> &btns)
-**/
-int events(nsEvent::EventManager &eventM, const vector<button> &btns)
-{
+int nsMenu::events(nsEvent::EventManager &eventM, const vector<Button> &btns) {
     // On vérifie chaque évènement de la queue d'évènements
     while (eventM.hasEvent())
     {
@@ -26,7 +13,7 @@ int events(nsEvent::EventManager &eventM, const vector<button> &btns)
         nsGraphics::Vec2D eventPos{data.x, data.y};
         if(event.eventType==nsEvent::EventType_t::MouseClick && !data.button && !data.state){
             //cout << endl << "A click : " << eventPos << endl;
-            for(const button & btn : btns) {
+            for(const Button & btn : btns) {
                 //cout << btn.content << " at pos : " << btn.rect.getFirstPosition() << " | " << btn.rect.getSecondPosition() << endl;
                 if (eventPos.isColliding(btn.rect.getFirstPosition(), btn.rect.getSecondPosition())){
                     //cout << btn.content << endl;
@@ -36,14 +23,9 @@ int events(nsEvent::EventManager &eventM, const vector<button> &btns)
         }
     }
     return -1;
-}//events
+} // events()
 
-/*!
-  * @brief fonction pour recuperer la derniere touche qui as été appuyé sur le clavier
-  * @param[in/out] window
-  * @fn char getLastPressedChar(MinGL &window)
-**/
-char getLastPressedChar(MinGL &window){
+char nsMenu::getLastPressedChar(MinGL &window) {
     string alphabet = "abcdefghijklmnopqrstuvwxyz";
     for(auto letter : alphabet){
         if (window.isPressed({letter, false})){
@@ -52,22 +34,15 @@ char getLastPressedChar(MinGL &window){
         }
     }
     return ' ';
-}//getLastPressedChar
+} // getLastPressedChar()
 
-/*!
-  * @brief fonction pour placer des boutons centrés horizontalement et verticalement
-  * @param[in/out] window : fenetre
-  * @param[in] b : vecteur de struct contenant le texte à afficher et la couleur dans laquelle l'afficher
-  * @param[in] width : largeur de la fenetre
-  * @param[in] height : hauteur de la fenetre
-**/
-void placeBtns(const MinGL &window, vector<button> &btns) {
+void nsMenu::placeBtns(const MinGL &window, vector<Button> &btns) {
 
     nsGraphics::Vec2D winSize = window.getWindowSize();
     size_t nb_btns = btns.size();
 
     unsigned text_max_size = 0;
-    for (const button &btn : btns)
+    for (const Button &btn : btns)
         if (btn.content.size() > text_max_size)
             text_max_size = btn.content.size();
     nsGraphics::Vec2D btnSize{8*text_max_size+64, 64};
@@ -84,25 +59,14 @@ void placeBtns(const MinGL &window, vector<button> &btns) {
 
     }
 
-}//placeBtns
+} // placeBtns()
 
-/*!
-  * @brief fonction utilisé pour afficher les boutons dans la fenetre : window
-  * @param[in/out] window
-  * @param[in] btns : vecteur contenant la liste des boutons à afficher
-  * @fn void drawBtns(MinGL &window, const vector<button> &btns)
-**/
-void drawBtns(MinGL &window, const vector<button> &btns) {
-    for (const button &btn : btns)
+void nsMenu::drawBtns(MinGL &window, const vector<Button> &btns) {
+    for (const Button &btn : btns)
         window << btn.rect << btn.text;
-}//DrawBtns
+} // drawBtns()
 
-/*!
-  * @brief fonction utilisé pour lire le fichier leaderBoard
-  * @param[in/out] leaderBoard : liste avec les noms des players et leur score, avec indice 0 pour le meilleur et 9 pour le pire
-  * @fn void getLeaderBoard(vector<string> &leaderBoard)
-**/
-void getLeaderBoard(vector<string> &leaderBoard){
+void nsMenu::getLeaderBoard(vector<string> &leaderBoard){
     //leaderBoard init to 10
     string score;
     ifstream scoreFile ("leaderBoard.txt");
@@ -110,14 +74,9 @@ void getLeaderBoard(vector<string> &leaderBoard){
         getline(scoreFile,score);
         (*iter)=score;
     }
-}//getLeaderBoard
+} // getLeaderBoard()
 
-/*!
-  * @brief fonction utilisé pour acceder au valeurs dans le fichier de configuration.yaml
-  * @param[in/out] dictionnaire avec les parametres
-  * @fn void readConfFile(map<string,string> &settings
-**/
-void readConfFile(map<string,string> &settings){
+void nsMenu::readConfFile(map<string,string> &settings) {
 
     ifstream configFile ("config.yaml");
     string line;
@@ -142,35 +101,25 @@ void readConfFile(map<string,string> &settings){
         }
         settings[key]=value;
     }
-}// readConfFile
+} // readConfFile()
 
-/*!
-  * @brief fonction utilisé pour ecrire les valeurs dans le dictionnaire parametres, dans le fichier de configuration.yaml
-  * @param[in/out] dictionnaire avec les parametres
-  * @fn void writeConfigFile(map<string,string> &settings)
-**/
-void writeConfigFile(map<string,string> &settings){
+void nsMenu::writeConfigFile(map<string,string> &settings) {
 
     ofstream ofs ("config.yaml");
     for(map<string,string>::iterator iter = settings.begin(); iter!=settings.end(); ++iter){
         ofs<<(*iter).first<<":"<<(*iter).second<<endl;
     }
     ofs.close();
-}//writeConfigFile
+} // writeConfigFile()
 
-/*!
-  * @brief fonction utilisé pour commencer le jeu
-  * @param[in/out] window
-  * @fn void startButton(MinGL &window
-**/
-void startButton(MinGL &window){
+void nsMenu::startButton(MinGL &window) {
     //initialisation des boutons du menu Start
-    vector<button> btns(5);
-    btns[0] = button {"Enter your Name :",0, nsGraphics::KGray, nsGraphics::KWhite, nsGraphics::KBlack};
-    btns[1] = button {" ",1, nsGraphics::KSilver, nsGraphics::KWhite, nsGraphics::KBlack};
-    btns[2] = button {"Delete last character",2, nsGraphics::KSilver, nsGraphics::KWhite, nsGraphics::KBlack};
-    btns[3] = button {"Play",-1, nsGraphics::KRed, nsGraphics::KWhite, nsGraphics::KBlack};
-    btns[4] = button {"Back",4, nsGraphics::KGray, nsGraphics::KWhite, nsGraphics::KBlack};
+    vector<Button> btns(5);
+    btns[0] = Button {"Enter your Name :",0, nsGraphics::KGray, nsGraphics::KWhite, nsGraphics::KBlack};
+    btns[1] = Button {" ",1, nsGraphics::KSilver, nsGraphics::KWhite, nsGraphics::KBlack};
+    btns[2] = Button {"Delete last character",2, nsGraphics::KSilver, nsGraphics::KWhite, nsGraphics::KBlack};
+    btns[3] = Button {"Play",-1, nsGraphics::KRed, nsGraphics::KWhite, nsGraphics::KBlack};
+    btns[4] = Button {"Back",4, nsGraphics::KGray, nsGraphics::KWhite, nsGraphics::KBlack};
     placeBtns(window, btns);
 
     //variables
@@ -181,7 +130,7 @@ void startButton(MinGL &window){
     window.getEventManager().clearEvents();
     chrono::microseconds frameTime = chrono::microseconds::zero();
 
-    while(flag){
+    while(flag) {
         // Récupère l'heure actuelle
         chrono::time_point<chrono::steady_clock> start = chrono::steady_clock::now();
 
@@ -235,23 +184,18 @@ void startButton(MinGL &window){
         frameTime = chrono::duration_cast<chrono::microseconds>(chrono::steady_clock::now() - start);
 
     }
-}//startButton
+} // startButton()
 
-/*!
-  * @brief fonction acceder au menu qui affiche la liste des meilleurs scores
-  * @param[in/out] window
-  * @fn void scoreButton(MinGL &window)
-**/
-void scoreButton(MinGL &window){
+void nsMenu::scoreButton(MinGL &window){
     vector<string> leaderBoard(10);
     getLeaderBoard(leaderBoard);
 
-    vector<button> btns(12);
-    btns[0] = button {"Leaderboard :",0, nsGraphics::KGray, nsGraphics::KWhite, nsGraphics::KBlack};
+    vector<Button> btns(12);
+    btns[0] = Button {"Leaderboard :",0, nsGraphics::KGray, nsGraphics::KWhite, nsGraphics::KBlack};
     for(unsigned i = 0 ; i<leaderBoard.size() ; ++i){
-        btns[i+1] = button {leaderBoard[i],(int)i+1, nsGraphics::KSilver, nsGraphics::KWhite, nsGraphics::KBlack};
+        btns[i+1] = Button {leaderBoard[i],(int)i+1, nsGraphics::KSilver, nsGraphics::KWhite, nsGraphics::KBlack};
     }
-    btns[11] = button {"Back",11, nsGraphics::KGray, nsGraphics::KWhite, nsGraphics::KBlack};
+    btns[11] = Button {"Back",11, nsGraphics::KGray, nsGraphics::KWhite, nsGraphics::KBlack};
     placeBtns(window, btns);
 
     bool flag=true;
@@ -281,14 +225,9 @@ void scoreButton(MinGL &window){
         frameTime = chrono::duration_cast<chrono::microseconds>(chrono::steady_clock::now() - start);
 
     }
-}//scoreButton
+} // scoreButton()
 
-/*!
-  * @brief fonction utilisé pour acceder au menu des reglages
-  * @param[in/out] window
-  * @fn void settingsButton(MinGL &window)
-**/
-void settingsButton(MinGL &window){
+void nsMenu::settingsButton(MinGL &window){
 
     //Dictionary containing the configs from the config file
     map<string,string> settings;
@@ -308,18 +247,18 @@ void settingsButton(MinGL &window){
     buttonContent[9]="Theme";
 
     //Init the button list
-    vector<button> btns(11);
-    btns[0] = button {"To change a key : press the wanted key on the keyboard,",-1, nsGraphics::KSilver, nsGraphics::KWhite, nsGraphics::KBlack};
-    btns[1] = button {"then select the concearning button containing the key",-1, nsGraphics::KSilver, nsGraphics::KWhite, nsGraphics::KBlack};
-    btns[2] = button {"Left Key :",-1, nsGraphics::KGray, nsGraphics::KWhite, nsGraphics::KBlack};
-    btns[3] = button {settings["LeftKey"],3, nsGraphics::KWhite, nsGraphics::KGray, nsGraphics::KBlack};
-    btns[4] = button {"Right Key :",-1, nsGraphics::KGray, nsGraphics::KGray, nsGraphics::KBlack};
-    btns[5] = button {settings["RightKey"],5, nsGraphics::KWhite, nsGraphics::KGray, nsGraphics::KBlack};
-    btns[6] = button {"Shoot Key :",-1, nsGraphics::KGray, nsGraphics::KGray, nsGraphics::KBlack};
-    btns[7] = button {settings["ShootKey"],7, nsGraphics::KWhite, nsGraphics::KGray, nsGraphics::KBlack};
-    btns[8] = button {"Theme :",-1, nsGraphics::KGray, nsGraphics::KGray, nsGraphics::KBlack};
-    btns[9] = button {themes[stoi( settings["Theme"] )],9, nsGraphics::KWhite, nsGraphics::KGray, nsGraphics::KBlack};
-    btns[10] = button {"Back",10, nsGraphics::KGray, nsGraphics::KWhite, nsGraphics::KBlack};
+    vector<Button> btns(11);
+    btns[0] = Button {"To change a key : press the wanted key on the keyboard,",-1, nsGraphics::KSilver, nsGraphics::KWhite, nsGraphics::KBlack};
+    btns[1] = Button {"then select the concearning button containing the key",-1, nsGraphics::KSilver, nsGraphics::KWhite, nsGraphics::KBlack};
+    btns[2] = Button {"Left Key :",-1, nsGraphics::KGray, nsGraphics::KWhite, nsGraphics::KBlack};
+    btns[3] = Button {settings["LeftKey"],3, nsGraphics::KWhite, nsGraphics::KGray, nsGraphics::KBlack};
+    btns[4] = Button {"Right Key :",-1, nsGraphics::KGray, nsGraphics::KGray, nsGraphics::KBlack};
+    btns[5] = Button {settings["RightKey"],5, nsGraphics::KWhite, nsGraphics::KGray, nsGraphics::KBlack};
+    btns[6] = Button {"Shoot Key :",-1, nsGraphics::KGray, nsGraphics::KGray, nsGraphics::KBlack};
+    btns[7] = Button {settings["ShootKey"],7, nsGraphics::KWhite, nsGraphics::KGray, nsGraphics::KBlack};
+    btns[8] = Button {"Theme :",-1, nsGraphics::KGray, nsGraphics::KGray, nsGraphics::KBlack};
+    btns[9] = Button {themes[stoi( settings["Theme"] )],9, nsGraphics::KWhite, nsGraphics::KGray, nsGraphics::KBlack};
+    btns[10] = Button {"Back",10, nsGraphics::KGray, nsGraphics::KWhite, nsGraphics::KBlack};
     placeBtns(window, btns);
 
     bool flag=true;
@@ -374,15 +313,9 @@ void settingsButton(MinGL &window){
         frameTime = chrono::duration_cast<chrono::microseconds>(chrono::steady_clock::now() - start);
     }
     writeConfigFile(settings);
-}//settingsButton
+} // settingsButton()
 
-/*!
-  * @brief fonction qui sert à verifier quel bouton as été appuyé sur le menu principale, pour changer de fenetre
-  * @param[in/out] window : fenetre
-  * @param[in/out] content : contenu du bouton appuyé
-  * @fn void checkContent(MinGL &window, int &content)
-**/
-void checkContent(MinGL &window, int &content){
+void nsMenu::checkContent(MinGL &window, int &content){
     switch(content) {
     case -1:
         break;
@@ -399,9 +332,9 @@ void checkContent(MinGL &window, int &content){
         window.~MinGL();
         break;
     }
-}//checkContent
+} // checkContent()
 
-int menu(){
+void nsMenu::menu() {
     // Initialise le système
     const unsigned width = 1280;
     const unsigned heigth = 720;
@@ -415,11 +348,11 @@ int menu(){
     window.getEventManager().clearEvents();
 
     // Création des boutons
-    vector<button> btns(4);
-    btns[0] = button {"Start",1, nsGraphics::KBlue, nsGraphics::KRed, nsGraphics::KLime};
-    btns[1] = button {"Scores",2, nsGraphics::KCyan, nsGraphics::KYellow, nsGraphics::KGreen};
-    btns[2] = button {"Settings",3, nsGraphics::KTeal, nsGraphics::KMaroon, nsGraphics::KWhite};
-    btns[3] = button {"Quit",4, nsGraphics::KGreen, nsGraphics::KMaroon, nsGraphics::KWhite};
+    vector<Button> btns(4);
+    btns[0] = Button {"Start",1, nsGraphics::KBlue, nsGraphics::KRed, nsGraphics::KLime};
+    btns[1] = Button {"Scores",2, nsGraphics::KCyan, nsGraphics::KYellow, nsGraphics::KGreen};
+    btns[2] = Button {"Settings",3, nsGraphics::KTeal, nsGraphics::KMaroon, nsGraphics::KWhite};
+    btns[3] = Button {"Quit",4, nsGraphics::KGreen, nsGraphics::KMaroon, nsGraphics::KWhite};
     placeBtns(window, btns);
 
     int idbutton;
@@ -449,4 +382,4 @@ int menu(){
         frameTime = chrono::duration_cast<chrono::microseconds>(chrono::steady_clock::now() - start);
     }
     window.getEventManager().clearEvents();
-}//menu
+} // menu()
--- menu.h
+++ menu.h
@@ -1,17 +1,9 @@
 #ifndef MENU_H
 #define MENU_H
 
-/*!
-  * @author Ceccarelli Luca - Clement Romain - Saadi Nils - Valls Marion
-  * @date 9 jan 2021
-  * @brief fichier permettant de créer des menus pour commencer le jeu
-**/
-
-#define FPS_LIMIT 60
-
 #include <iostream>
 #include <thread>   //gerer les frames et le temps
-#include <string>   //convert char type to string
+#include <string>   //convert char type to std::string
 #include <map>      //store game themes
 #include <fstream>  //read leaderBoard File
 
@@ -22,16 +14,16 @@
 #include "mingl/shape/line.h"
 #include "mingl/event/event.hpp"
 
-using namespace std;
+namespace nsMenu {
 
 /*!
-  * @brief structure contenant l'element button
-  * @fn button{string content, int buttonId,  nsGraphics::RGBAcolor bgColor,  nsGraphics::RGBAcolor borderColor,  nsGraphics::RGBAcolor textColor }
+  * @struct structure contenant l'element button
+  * @fn button{std::string content, int buttonId,  nsGraphics::RGBAcolor bgColor,  nsGraphics::RGBAcolor borderColor,  nsGraphics::RGBAcolor textColor }
 **/
-struct button{
+struct Button {
     //texte a afficher et couleur dans laquelle l'afficher
 
-    string content;
+    std::string content;
     int buttonId;
     nsGraphics::RGBAcolor bgColor;
     nsGraphics::RGBAcolor borderColor;
@@ -47,15 +39,15 @@ struct button{
                             nsGraphics::Vec2D{0,0},
                             bgColor,
                             borderColor};
-};//button
+}; // struct Button
 
 /*!
   * @brief fonction pour verifier si on appuye sur la fenetre de jeu et si un bouton est concerné
   * @param[in/out] EventManager
   * @param[in] btns : vecteur contenant les boutons
-  * @fn int events(nsEvent::EventManager &eventM, const vector<button> &btns)
+  * @fn int events(nsEvent::EventManager &eventM, const std::vector<button> &btns)
 **/
-int events(nsEvent::EventManager &eventM, const vector<button> &btns);
+int events(nsEvent::EventManager &eventM, const std::vector<Button> &btns);
 
 /*!
   * @brief fonction pour recuperer la derniere touche qui as été appuyé sur le clavier
@@ -71,55 +63,55 @@ char getLastPressedChar(MinGL &window);
   * @param[in] width : largeur de la fenetre
   * @param[in] height : hauteur de la fenetre
 **/
-void placeBtns(const MinGL &window, vector<button> &btns);
+void placeBtns(const MinGL &window, std::vector<Button> &btns);
 
 /*!
   * @brief fonction utilisé pour afficher les boutons dans la fenetre : window
   * @param[in/out] window
   * @param[in] btns : vecteur contenant la liste des boutons à afficher
-  * @fn void drawBtns(MinGL &window, const vector<button> &btns)
+  * @fn void drawBtns(MinGL &window, const std::vector<button> &btns)
 **/
-void drawBtns(MinGL &window, const vector<button> &btns);
+void drawBtns(MinGL &window, const std::vector<Button> &btns);
 
 /*!
   * @brief fonction utilisé pour lire le fichier leaderBoard
   * @param[in/out] leaderBoard : liste avec les noms des players et leur score, avec indice 0 pour le meilleur et 9 pour le pire
-  * @fn void getLeaderBoard(vector<string> &leaderBoard)
+  * @fn void getLeaderBoard(std::vector<std::string> &leaderBoard)
 **/
-void getLeaderBoard(vector<string> &leaderBoard);
+void getLeaderBoard(std::vector<std::string> &leaderBoard);
 
 /*!
   * @brief fonction utilisé pour acceder au valeurs dans le fichier de configuration.yaml
   * @param[in/out] dictionnaire avec les parametres
-  * @fn void readConfFile(map<string,string> &settings
+  * @fn void readConfFile(map<std::string,std::string> &settings;
 **/
-void readConfFile(map<string,string> &settings);
+void readConfFile(std::map<std::string,std::string> &settings);
 
 /*!
   * @brief fonction utilisé pour ecrire les valeurs dans le dictionnaire parametres, dans le fichier de configuration.yaml
   * @param[in/out] dictionnaire avec les parametres
-  * @fn void writeConfigFile(map<string,string> &settings)
+  * @fn void writeConfigFile(map<std::string,std::string> &settings);
 **/
-void writeConfigFile(map<string,string> &settings);
+void writeConfigFile(std::map<std::string,std::string> &settings);
 
 /*!
   * @brief fonction utilisé pour commencer le jeu
   * @param[in/out] window
   * @fn void startButton(MinGL &window
 **/
-void startButton(MinGL &window);
+void startButton(MinGL &window);;
 
 /*!
   * @brief fonction acceder au menu qui affiche la liste des meilleurs scores
   * @param[in/out] window
-  * @fn void scoreButton(MinGL &window)
+  * @fn void scoreButton(MinGL &window);
 **/
 void scoreButton(MinGL &window);
 
 /*!
   * @brief fonction utilisé pour acceder au menu des reglages
   * @param[in/out] window
-  * @fn void settingsButton(MinGL &window)
+  * @fn void settingsButton(MinGL &window);
 **/
 void settingsButton(MinGL &window);
 
@@ -132,10 +124,11 @@ void settingsButton(MinGL &window);
 void checkContent(MinGL &window, int &content);
 
 /*!
-  * @brief fonction pour lancer le menu principale
+  * @brief fonction pour lancer le menu principal
   * @fn void menu()
 **/
 void menu();
 
+} // namespace nsMenu
 
 #endif // MENU_H
