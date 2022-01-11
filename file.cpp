3742ae1d3d7956bb1b694fc10b7451a02efc4f1d
lucaceccarelli <91269970+lucaceccarelli@users.noreply.github.com>
2022-01-11 17:17:50 +0100

    Create file.cpp

---
+++ file.cpp
@@ -0,0 +1,106 @@
+#include "file.h"
+
+using namespace std;
+
+void nsFile::getLeaderBoard(vector<string> &leaderBoard){
+    //leaderBoard init to 10
+    string score;
+    ifstream scoreFile ("leaderBoard.txt");
+    for(vector<string>::iterator iter = leaderBoard.begin(); iter !=leaderBoard.end(); ++iter){
+        getline(scoreFile,score);
+        (*iter)=score;
+    }
+} // getLeaderBoard()
+
+void nsFile::addScore(vector<string> &leaderBoard,string username, unsigned score){
+    vector<unsigned> leaderBoardScores(10);
+    vector<string> leaderBoardUsernames(10);
+
+    unsigned i = 0;
+
+    for(string &line : leaderBoard){
+
+        string value="";
+        bool isSeparatorfound = false;
+
+        for(char &letter : line){
+            if(letter == ':'){
+                isSeparatorfound = true;
+                continue;
+            }
+            if(isSeparatorfound){
+                value+=string(1,letter);
+            }
+            else{
+                leaderBoardUsernames[i]+=string(1,letter);
+            }
+        }
+        if(isSeparatorfound){
+            leaderBoardScores[i]=stoul(value);
+        }
+        ++i;
+    }
+    unsigned place;
+    bool hasBeenAdded = false;
+    for(unsigned j =0; j<leaderBoardScores.size(); ++j){
+        if(leaderBoardScores[j]<score){
+            place = j;
+            hasBeenAdded = true;
+            break;
+        }
+    }
+    if(hasBeenAdded){
+        for(unsigned j = leaderBoard.size()-1; j>place; --j){
+            leaderBoardScores[j]=leaderBoardScores[j-1];
+            leaderBoardUsernames[j]=leaderBoardUsernames[j-1];
+        }
+        leaderBoardScores[place]=score;
+        leaderBoardUsernames[place]=username;
+        for(unsigned j =0; j<leaderBoard.size(); ++j){
+            leaderBoard[j]=leaderBoardUsernames[j]+":"+to_string(leaderBoardScores[j]);
+        }
+    }
+}//addScore()
+
+void nsFile::writeLeaderBoard(vector<string> &leaderBoard){
+    ofstream scoreFile ("leaderBoard.txt");
+    for(vector<string>::iterator iter = leaderBoard.begin() ; iter<leaderBoard.end(); ++iter){
+        scoreFile<<*iter<<endl;
+    }
+}//writeLeaderBoard()
+
+void nsFile::readConfFile(map<string,string> &settings) {
+
+    ifstream configFile ("config.yaml");
+    string line;
+
+    while(getline(configFile,line)){
+
+        string value="";
+        string key="";
+        bool isSeparatorfound = false;
+
+        for(char letter : line){
+            if(letter == ':'){
+                isSeparatorfound = true;
+                continue;
+            }
+            if(isSeparatorfound){
+                value+=string(1,letter);
+            }
+            else{
+                key+=string(1,letter);
+            }
+        }
+        settings[key]=value;
+    }
+} // readConfFile()
+
+void nsFile::writeConfigFile(map<string,string> &settings) {
+
+    ofstream ofs ("config.yaml");
+    for(map<string,string>::iterator iter = settings.begin(); iter!=settings.end(); ++iter){
+        ofs<<(*iter).first<<":"<<(*iter).second<<endl;
+    }
+    ofs.close();
+} // writeConfigFile()
